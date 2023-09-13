81340821e3083c68dc0f4b271fc298643ddcf418
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-09-13 14:03:27 -0600

    #2 adjust test for home controller

--- test/passport_web/controllers/page_controller_test.exs
+++ test/passport_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule PassportWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Todos por el café"
   end
 end
