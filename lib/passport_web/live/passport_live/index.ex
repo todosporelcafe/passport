bd3428bce805b02bfafb007608fac352241caac9
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-09-13 18:09:17 -0600

    #8 add support for uploading files!

--- lib/passport_web/live/passport_live/index.ex
+++ lib/passport_web/live/passport_live/index.ex
@@ -2,6 +2,35 @@ defmodule PassportWeb.PassportLive.Index do
   use PassportWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok,
+     socket
+     |> assign(:uploaded_files, [])
+     |> allow_upload(:passport, accept: ~w(.jpg .jpeg .png), max_entries: 1)}
   end
+
+  def handle_event("validate", unsigned_params, socket) do
+    IO.inspect(binding())
+    {:noreply, socket}
+  end
+
+  def handle_event("cancel-upload", %{"ref" => ref}, socket) do
+    {:noreply, cancel_upload(socket, :passport, ref)}
+  end
+
+  def handle_event("save", _params, socket) do
+    uploaded_files =
+      consume_uploaded_entries(socket, :passport, fn %{path: path}, _entry ->
+        dest = Path.join([:code.priv_dir(:passport), "static", "uploads", Path.basename(path)])
+        # The `static/uploads` directory must exist for `File.cp!/2`
+        # and MyAppWeb.static_paths/0 should contain uploads to work,.
+        File.cp!(path, dest)
+        {:ok, ~p"/uploads/#{Path.basename(dest)}"}
+      end)
+
+    {:noreply, update(socket, :uploaded_files, &(&1 ++ uploaded_files))}
+  end
+
+  def error_to_string(:too_large), do: "Too large"
+  def error_to_string(:not_accepted), do: "You have selected an unacceptable file type"
+  def error_to_string(:too_many_files), do: "You have selected too many files"
 end
