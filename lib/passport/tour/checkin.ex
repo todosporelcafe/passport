ceefe55b32321541bbe880b9edee245a04673a37
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-10-02 17:10:01 -0600

    #5 use full module

--- lib/passport/tour/checkin.ex
+++ lib/passport/tour/checkin.ex
@@ -1,16 +1,14 @@
 defmodule Passport.Tour.Checkin do
   use Ecto.Schema
   import Ecto.Changeset
-  alias Passport.Accounts.User
-  alias Passport.Tour.SpecialtyBar
 
   @fields ~w/specialty_bar_id user_id/a
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "checkins" do
-    belongs_to :user, User
-    belongs_to :specialty_bar, SpecialtyBar
+    belongs_to :user, Passport.Accounts.User
+    belongs_to :specialty_bar, Passport.Tour.SpecialtyBar
 
     timestamps()
   end
