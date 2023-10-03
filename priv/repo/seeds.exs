84f86acee8902dc0ef059d89ef8c7d4c00e9a233
Hector Garcia Rosales <ghectormus@gmail.com>
2023-10-02 21:06:54 -0600

    using insert and delete_all for schema specialty_bars

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,9 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+#
+alias Passport.Repo
+alias Passport.Tour.SpecialtyBar
+
+Repo.delete_all SpecialtyBar
+Repo.insert! %SpecialtyBar{ name: "Cafe Galeno" }
