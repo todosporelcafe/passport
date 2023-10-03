509ce2a0bbc4e15c1ee83729963eb44de9484bfc
Hector Garcia Rosales <ghectormus@gmail.com>
2023-10-02 22:47:48 -0600

    refac using a list of bars, problem with description size

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -14,4 +14,37 @@ alias Passport.Repo
 alias Passport.Tour.SpecialtyBar
 
 Repo.delete_all SpecialtyBar
-Repo.insert! %SpecialtyBar{ name: "Cafe Galeno" }
+bars = [
+
+  %SpecialtyBar{
+    name: "Babüin Café",
+    address: "C. Bajío 303, Roma Sur, Cuauhtémoc, 06760 Ciudad de México, CDMX, México",
+    description: "ace en los momentos más inesperados y por las conexiones humanas que enriquecen nuestra existencia.",
+    instagram: "https://www.instagram.com/babuincafe/",
+    tiktok: "https://www.instagram.com/babuincafe/",
+    facebook: "https://www.facebook.com/babuincafe"
+  },
+
+  %SpecialtyBar{
+    name: "Bar2 Name",
+    address: "Bar2 Address",
+    description: "Bar2 Description",
+    instagram: "bar2_instagram",
+    tiktok: "bar2_tiktok",
+    facebook: "bar2_facebook"
+  },
+
+  %SpecialtyBar{
+    name: "Bar3 Name",
+    address: "Bar3 Address",
+    description: "Bar3 Description",
+    instagram: "bar3_instagram",
+    tiktok: "bar3_tiktok",
+    facebook: "bar3_facebook"
+  }
+
+
+
+  ]
+
+Enum.each(bars, &Repo.insert!(&1))
