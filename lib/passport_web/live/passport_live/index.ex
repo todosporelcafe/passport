587b286af251c20dcea1d66e6b32fcc59b45c96d
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-10-05 00:02:13 -0600

    #8 use simple_form and handle event for validates

--- lib/passport_web/live/passport_live/index.ex
+++ lib/passport_web/live/passport_live/index.ex
@@ -1,16 +1,24 @@
 defmodule PassportWeb.PassportLive.Index do
   use PassportWeb, :live_view
+  alias Passport.Tour
+  alias Passport.Tour.PhysicalDocument
 
   def mount(_params, _session, socket) do
+    changeset = Tour.change_physical_document(%PhysicalDocument{})
+
     {:ok,
      socket
-     |> assign(:uploaded_files, [])
+     |> assign_form(changeset)
      |> allow_upload(:passport, accept: ~w(.jpg .jpeg .png), max_entries: 1)}
   end
 
-  def handle_event("validate", unsigned_params, socket) do
-    IO.inspect(binding())
-    {:noreply, socket}
+  def handle_event("validate", %{"physical_document" => physical_document_params}, socket) do
+    physical_document_changeset =
+      %PhysicalDocument{}
+      |> Tour.change_physical_document(physical_document_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign_form(socket, physical_document_changeset)}
   end
 
   def handle_event("cancel-upload", %{"ref" => ref}, socket) do
@@ -18,7 +26,7 @@ defmodule PassportWeb.PassportLive.Index do
   end
 
   def handle_event("save", _params, socket) do
-    uploaded_files =
+    _uploaded_files =
       consume_uploaded_entries(socket, :passport, fn %{path: path},
                                                      %Phoenix.LiveView.UploadEntry{
                                                        client_name: client_name
@@ -28,13 +36,17 @@ defmodule PassportWeb.PassportLive.Index do
         # and MyAppWeb.static_paths/0 should contain uploads to work,.
 
         File.cp!(path, dest)
-        {:ok, ~p"/uploads/#{dest}"}
+        {:ok, "/uploads/#{dest}"}
       end)
 
-    {:noreply, update(socket, :uploaded_files, &(&1 ++ uploaded_files))}
+    {:noreply, socket}
   end
 
   def error_to_string(:too_large), do: "Too large"
   def error_to_string(:not_accepted), do: "You have selected an unacceptable file type"
   def error_to_string(:too_many_files), do: "You have selected too many files"
+
+  defp assign_form(socket, %Ecto.Changeset{} = changeset) do
+    assign(socket, :form, to_form(changeset))
+  end
 end
